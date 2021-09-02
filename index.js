48fe211e44b50c40bb0d2ccbff41dfafe529d4e8
Leu Family <romanianfobjr@gmail.com>
2021-09-02 11:58:09 -0500

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
 
-server.listen(5000, () => {
-    console.log('listening on port 5000')
+// console.log(process.env.PORT) // does not work on my machine, but should work on heroku
+
+const PORT = process.env.PORT || 5000 // genius
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Web 45 Rocks</h1>`)
+})
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'Web 45 is awesome' })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
